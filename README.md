4908f13dfd74d954b9d81a47194012165978cb86
LE NGUYEN Huu Tri <hlenguyen1-amaris@outlook.com>
2018-12-10 17:41:10 +0700

    ~ add loading comp ~ update readme

--- README.md
+++ README.md
@@ -5,9 +5,30 @@ https://trilnh92.surge.sh
 ### Content
 This my place to display about me, portfolio, contact and many more
 
+### To-do list
+- [x] Init React project
+- [x] Routes
+- [ ] Add Bootstrap 4
+- [ ] Add Font Awesome 5
+- [ ] Define common stylesheet
+- [ ] Header
+- [ ] Menu
+- [ ] Homepage
+- [ ] About page
+- [ ] Portfolio page
+- [ ] About page
+- [ ] Server Side Rendering
+- [ ] Code Splitting
+- [ ] State Management
+- [ ] Progressive Web Application
+- [ ] Critical CSS
+
+
 ### Technologies
 - HTML
 - CSS / SCSS
 - React
 
 ### Project Folder Structure
+
+
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,15 +2,16 @@ import React, {Suspense, lazy} from 'react';
 import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 
 import {AuthService} from "../services/AuthService"
+import Homepage from "../container/Homepage/Homepage";
+import Layout from "../components/Layout/Layout";
 
-const Homepage = lazy(() => import('../container/Homepage/Homepage'));
 const About = lazy(() => import('../container/About/About'));
 const Portfolio = lazy(() => import('../container/Portfolio/Portfolio'));
 const Login = lazy(() => import('../container/Login/Login'));
 
 const ComingSoon = lazy(() => import('../components/ComingSoon/ComingSoon'));
 const Errors = lazy(() => import('../components/Errors/Errors'));
-const Layout = lazy(() => import('../components/Layout/Layout'));
+
 
 // TODO: implement authenticated route
 //https://medium.freecodecamp.org/beginners-guide-to-react-router-4-8959ceb3ad58
@@ -31,7 +32,10 @@ const RoutingConfig = () => (
         <Suspense fallback={<div>Loading...</div>}>
             <Layout>
                 <Switch>
-                    <Route path="/" exact={true} component={props => <Homepage {...props} />}/>
+                    <Route
+                        path="/" exact
+                        render={props => <Homepage {...props}  />}
+                    />
                     <Route path="/about"  component={props => <About {...props} />}/>
                     <Route path="/portfolio"  component={props => <Portfolio {...props} />}/>
                     <Route path="/coming-soon"  component={props => <ComingSoon {...props} />}/>
